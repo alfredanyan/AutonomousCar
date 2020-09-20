3fe9d214e2ff4403b9a55e73de91945f4ac915fb
Alfred Anyan <46558691+yawaddison@users.noreply.github.com>
2020-09-20 10:00:48 +0200

    Jupyter Localization file

--- Localization/Localization.py
+++ Localization/Localization.py
@@ -1,8 +1,13 @@
 p = [0.2,0.2,0.2,0.2,0.2]
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 world =["green","red","red","green","green"]
 measurements=["red","green"]
+motions = [1,1]
+
 
 def sense(p,Z):
     q=[]
@@ -14,7 +19,17 @@ def sense(p,Z):
         q[i] = q[i]/s
     return q
 
-for k in range(len(measurements)):
-    p=sense(p, measurements[k])
+# for k in range(len(measurements)):
+#     p=sense(p, measurements[k])
 
-print(sense(p, measurements))
+# print(sense(p, measurements))
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
+
